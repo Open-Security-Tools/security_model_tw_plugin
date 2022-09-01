1d763583a251e848c7ebf11384116a1b8ea309b6
paul <paul@opensecuritytools.org>
2022-09-01 22:19:34 +0100

    Tree tests. Spotted issue with Third Flow plugin - the file system adapter breaks the auto switching to JSON format for tiddlers with multi-line fields. Makes testing trees challenging

---
+++ src/tiddlers/system/plugins/security_tools/twsm/modules/twsm_trees.js
@@ -0,0 +1,66 @@
+/*\
+created: 20210723152326655
+type: application/javascript
+title: $:/plugins/security_tools/twsm/twsm_trees.js
+tags: 
+modified: 20220723070445305
+module-type: filteroperator
+\*/
+
+(function(){
+
+"use strict";
+
+function parse_attack_tree(tiddler, title) {
+    if (!tiddler) {
+        return;
+    }
+    if (tiddler.fields.twsm_class !== "risk") {
+        return;
+    }
+
+    var obj = {
+        computed_attack_tree: tiddler.fields.attack_tree,
+        likelihood: "0.245",
+        controls: "[[Control1]] [[Control2]] [[Control3]]",
+        attack_sub_trees: "[[Attack Sub Tree 1]] [[Attack Sub Tree 2]] [[Attack Sub Tree 3]]" 
+    }
+    return obj;
+}
+
+exports.twsmprocesstree = function(source, operator, options) {
+    var result = [];
+
+    source (function(tiddler, title) {
+        var obj = parse_attack_tree(tiddler, title);
+        if (obj) {
+            result.push(JSON.stringify(obj));
+        }
+    })
+    return result;
+}
+
+exports.twsmextractcontrols = function(source, operator, options) {
+    var result = [];
+
+    source (function(tiddler, title) {
+        var s = JSON.parse(title);
+        result.push(s.controls);
+    })
+    return result;
+}
+
+
+exports.twsmextractcomputedattacktree = function(source, operator, options) {
+    var result = [];
+
+    source (function(tiddler, title) {
+        var s = JSON.parse(title);
+        result.push(s.computed_attack_tree);
+    })
+    return result;
+}
+
+
+
+})();
