587823c4b5c7ed6876d3148c1c600d0eeca30314
Paul <paul@opensecuritytools.org>
2022-09-06 07:59:51 +0100

    parsing types in

--- plugins/security_tools/twsm/tiddlers/modules/twsm_trees.js
+++ plugins/security_tools/twsm/tiddlers/modules/twsm_trees.js
@@ -30,13 +30,11 @@ function eatWhiteSpace(x) {
     return lstrip(x, " \t");
 }
 
-function AttackTreeSyntaxError(message) {
-    const error = new Error(message);
-    return error;
+class AttackTreeSyntaxError extends Error {
+    constructor(message) {
+        super(message);
+    }
 }
-
-AttackTreeSyntaxError.prototype = Object.create(Error.prototype);
-
 
 function parseMacro(line) {
     var result = [];
@@ -44,6 +42,99 @@ function parseMacro(line) {
         return result;
     }
     return shlex.twsm_split(line.slice(2, -2));
+}
+
+var branchOperators = {
+    "OR": function() {
+
+    },
+    "AND": function() {
+
+    }
+}
+
+class Branch {
+    constructor(parent, branchName, indent, operator="OR") {
+        var operatorFunction = branchOperators[operator];
+        if (!operatorFunction) {
+            throw new AttackTreeSyntaxError("Unsupported operator (" + operator + ")");
+        }
+
+        this.parent = parent;
+        this.branchName = branchName;
+        this.operator = operator;
+        this.indent = indent;
+        this.children = [];
+    }
+
+    console_log() {
+        console.log("Branch: (" + this.indent + "): " + this.branchName);
+        for (let c of this.children) {
+            // console.log("Child: " + JSON.stringify(c));
+            c.console_log();
+        }
+    }
+}
+
+class Leaf {
+    constructor(parent, leafName, indent, probability="almost certain") {
+        this.parent = parent;
+        this.leafName = leafName;
+        this.indent = indent;
+        this.probability = probability;
+    }
+
+    console_log() {
+        console.log("Leaf (" + this.indent + "): " + this.leafName);
+    }
+}
+
+
+class Control {
+    constructor(parent, controlName, indent) {
+        if (!is_control(controlName)) {
+            throw new AttackTreeSyntaxError("Not a control!");
+        }
+        this.parent = parent;
+        this.controlName = controlName;
+        this.indent = indent;
+    }
+    console_log() {
+        console.log("Control (" + this.indent + "): " + this.controlName);
+    }
+}
+
+
+class Ref {
+    constructor(parent, refName, indent) {
+        if (!is_ref(refName)) {
+            throw new AttackTreeSyntaxError("Not a ref!");
+        }
+        this.parent = parent;
+        this.refName = refName;
+        this.indent = indent;
+    }
+    console_log() {
+        console.log("Ref (" + this.indent + "): " + this.refName);
+    }
+}
+
+
+function tw_filter_to_bool(filter) {
+    // console.log(filter);
+    return $tw.wiki.filterTiddlers(filter)[0] === "True";
+}
+
+function is_control(controlTitle) {
+    return tw_filter_to_bool(
+        "[title[" + controlTitle + "]twsm_class[control]then[True]else[False]]"
+    )
+}
+
+function is_ref(refTitle) {
+    return tw_filter_to_bool(
+        "[title[" + refTitle + "]twsm_class[attack_tree]then[True]else[False]]"
+    )
 }
 
 function parse_attack_tree(tiddler, title) {
@@ -58,32 +149,42 @@ function parse_attack_tree(tiddler, title) {
         return;
     }
 
-    var branchOperators = {
-        "OR": function() {
-
-        },
-        "AND": function() {
-
-        }
-    }
+    var controls = [];
+    var attack_sub_trees = [];
+    var root = new Branch(null, "Root", 0);
+    var currentBranch = root;
 
     var ops = {
         "branch": function(indent, args) {
             var branchName = args[0];
-            console.log("Args 1: " + args[1]);
-            var operatorFunction = branchOperators[args[1] || "OR"];
-            console.log("Operator function: " + operatorFunction);
-            if (!operatorFunction) {
-                throw new AttackTreeSyntaxError("Unsupported operator (" + args[1] + ")");
-            }
-            var operator = args[1] || "OR";
+            var operator = args[1];
+            var branch = new Branch(currentBranch, branchName, indent, operator);
+            console.log("B: " + branch);
+            currentBranch.children.push(branch);
+            currentBranch = branch;
+
             return indentToBullet(indent) + "BRANCH '" + branchName + "' - " + operator;
         },
         "leaf": function(indent, args) {
-            return indentToBullet(indent) + "LEAF" + args;
+            var leafName = args[0];
+            var probability = args[1];
+            var leaf = new Leaf(currentBranch, leafName, indent, probability);
+            currentBranch.children.push(leaf);
+            return indentToBullet(indent) + "LEAF " + args;
         },
         "control": function(indent, args) {
-            return indentToBullet(indent) + "CONTROL" + args;
+            var controlName = args[0];
+            var control = new Control(currentBranch, controlName, indent);
+            currentBranch.children.push(control);
+            controls.push(controlName);
+            return indentToBullet(indent) + "CONTROL " + args;
+        },
+        "ref": function(indent, args) {
+            var refName = args[0];
+            var ref = new Ref(currentBranch, refName, indent);
+            currentBranch.children.push(ref);
+            attack_sub_trees.push(refName);
+            return indentToBullet(indent) + "REF " + args;
         }
     }
 
@@ -95,10 +196,22 @@ function parse_attack_tree(tiddler, title) {
         try {
             var t = lstrip(l, "*")
             var indent = l.length - t.length;
+            if (!indent) {
+                throw new AttackTreeSyntaxError("Line doesn't start with a '*'!");
+            }
             if (indent) {
+                // Walk back up the tree finding the correct parent.
+                while (indent > (currentBranch.indent + 1)) {
+                    currentBranch = currentBranch.parent;
+                }
                 var macroArgs = parseMacro(t.trim());
-                console.log("macroArgs: " + macroArgs);
+                if (macroArgs.length === 0) {
+                    throw new AttackTreeSyntaxError("Missing macro prefix!");
+                }
                 var opFunc = ops[macroArgs[0]];
+                if (!opFunc) {
+                    throw new AttackTreeSyntaxError("Unsupported macro!");
+                }
                 if (opFunc) {
                     var r = opFunc(indent, macroArgs.slice(1,));
                     if (r) {
@@ -121,6 +234,7 @@ function parse_attack_tree(tiddler, title) {
         }
         lineNo += 1;
     }
+    root.console_log();
     var joined = newLines.join('\n');
     console.log("Joined: " + joined);
     // if (error) {
@@ -131,8 +245,8 @@ function parse_attack_tree(tiddler, title) {
         computed_attack_tree: joined,
         error: error,
         likelihood: "0.245",
-        controls: "[[Control1]] [[Control2]] [[Control3]]",
-        attack_sub_trees: "[[Attack Sub Tree 1]] [[Attack Sub Tree 2]] [[Attack Sub Tree 3]]" 
+        controls: controls,
+        attack_sub_trees: attack_sub_trees, 
     }
     return obj;
 }
@@ -153,7 +267,7 @@ exports.twsmprocesstree = function(source, operator, options) {
                 }));
             } else {
                 // result.push(JSON.stringify({
-                //     "error": objError.message
+                //     "error-monke": objError.message
                 // }));
                 throw(objError);
             }        
