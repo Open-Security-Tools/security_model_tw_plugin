4e25cf6f0893e35bb54427e9a2e78b22967ced78
Paul <paul@opensecuritytools.org>
2022-09-10 19:38:13 +0100

    added utils

---
+++ plugins/security_tools/twsm/tiddlers/modules/utils.js
@@ -0,0 +1,17 @@
+/*\
+created: 20220722215603976
+title: $:/plugins/security_tools/twsm/utils.js
+type: application/javascript
+tags: 
+modified: 20220723070445305
+module-type: library
+\*/
+
+(function(){
+
+"use strict";
+
+
+
+
+})();
