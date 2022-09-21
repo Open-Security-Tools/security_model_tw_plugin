dcfcfaec3c618a669fa31f7019f9b8a5afd5c162
paul <paul@opensecuritytools.org>
2022-09-21 20:27:25 +0100

    starting class

--- plugins/security_tools/twsm/tiddlers/modules/actions.js
+++ plugins/security_tools/twsm/tiddlers/modules/actions.js
@@ -16,6 +16,38 @@ var attack_utils = require("$:/plugins/security_tools/twsm/attack_utils.js");
 var Widget = require("$:/core/modules/widgets/widget.js").widget;
 var utils = require("$:/plugins/security_tools/twsm/utils.js");
 
+class ModelIntegrity {
+    constructor() {
+        this.updatedNodes = [];
+    }
+
+    updateControl(tiddler) {
+
+    }
+
+    updateRisk(tiddler) {
+
+    }
+
+    updateAttack(tiddler) {
+
+    }
+
+    updateEntity(tiddler) {
+        if (!tiddler.fields) {
+            return;
+        }
+        if (tiddler.fields.twsm_class === "control") {
+            this.updateControl(tiddler);
+        } else if (tiddler.fields.twsm_class === "risk") {
+            this.updateRisk(tiddler);
+        } else if (tiddler.fields.twsm_class === "attack") {
+            this.updateAttack(tiddler);
+        }
+    }
+}
+
+
 var UpdateRiskWidget = function(parseTreeNode,options) {
     this.initialise(parseTreeNode,options);
 };
@@ -24,11 +56,16 @@ var UpdateAttackWidget = function(parseTreeNode,options) {
     this.initialise(parseTreeNode,options);
 };
 
+var UpdateControlWidget = function(parseTreeNode,options) {
+    this.initialise(parseTreeNode,options);
+};
+
 /*
 Inherit from the base widget class
 */
 UpdateRiskWidget.prototype = new Widget();
 UpdateAttackWidget.prototype = new Widget();
+UpdateControlWidget.prototype = new Widget();
 
 /*
 Render this widget into the DOM
@@ -38,6 +75,10 @@ UpdateRiskWidget.prototype.render = function(parent,nextSibling) {
     this.execute();
 };
 UpdateAttackWidget.prototype.render = function(parent,nextSibling) {
+    this.computeAttributes();
+    this.execute();
+};
+UpdateControlWidget.prototype.render = function(parent,nextSibling) {
     this.computeAttributes();
     this.execute();
 };
@@ -53,6 +94,10 @@ UpdateAttackWidget.prototype.execute = function() {
     this.actionTiddler = this.getAttribute("$tiddler") || (!this.hasParseTreeNodeAttribute("$tiddler") && this.getVariable("currentTiddler"));
     this.actionTimestamp = this.getAttribute("$timestamp","yes") === "yes";
 };
+UpdateControlWidget.prototype.execute = function() {
+    this.actionTiddler = this.getAttribute("$tiddler") || (!this.hasParseTreeNodeAttribute("$tiddler") && this.getVariable("currentTiddler"));
+    this.actionTimestamp = this.getAttribute("$timestamp","yes") === "yes";
+};
 
 /*
 Refresh the widget by ensuring our attributes are up to date
@@ -62,6 +107,10 @@ UpdateRiskWidget.prototype.refresh = function(changedTiddlers) {
     return this.refreshChildren(changedTiddlers);
 };
 UpdateAttackWidget.prototype.refresh = function(changedTiddlers) {
+    // Nothing to refresh
+    return this.refreshChildren(changedTiddlers);
+};
+UpdateControlWidget.prototype.refresh = function(changedTiddlers) {
     // Nothing to refresh
     return this.refreshChildren(changedTiddlers);
 };
@@ -124,8 +173,29 @@ UpdateAttackWidget.prototype.invokeAction = function(triggeringWidget,event) {
     return true; // Action was invoked
 };
 
+UpdateControlWidget.prototype.invokeAction = function(triggeringWidget,event) {
+    var self = this,
+        options = {};
+
+    if(this.actionTiddler) {
+        var tiddler = $tw.wiki.getTiddler(this.actionTiddler);
+        if (tiddler && tiddler.fields.twsm_class === "control") {
+
+            // Find the objects which reference this control and update them recursively
+            // Risks and attacks reference this control in 'controls' field.
+
+
+        }
+    }
+    return true; // Action was invoked
+};
+
+
 exports["action-updaterisk"] = UpdateRiskWidget;
 
 exports["action-updateattack"] = UpdateAttackWidget;
+
+exports["action-updatecontrol"] = UpdateControlWidget;
+
 
 })();
