68584a121fed6aa7542c4fb3f8248dc3fd17bf06
paul <paul@opensecuritytools.org>
2022-10-03 08:40:26 +0100

    theme visualisation done

--- plugins/security_tools/twsm/tiddlers/test_data2.js
+++ plugins/security_tools/twsm/tiddlers/test_data2.js
@@ -8,11 +8,30 @@ module-type: echarts-component
 echarts-refresh-trigger: [twsm_class[theme]twsm_security_score[]twsm_json_field:score[]]
 \*/
 
+JSON.safeStringify = (obj, indent = 2) => {
+  let cache = [];
+  const retVal = JSON.stringify(
+    obj,
+    (key, value) =>
+      typeof value === "object" && value !== null
+        ? cache.includes(value)
+          ? undefined // Duplicate reference found, discard key
+          : cache.push(value) && value // Store value in our collection
+        : value,
+    indent
+  );
+  cache = null;
+  return retVal;
+};
+
 exports.onMount = function (echart) {
   var state = {};
   echart.on("click", function (event) {
-    console.log("On click: " + event.data.name);
-    new $tw.Story().navigateTiddler(event.data.name);
+    if (event.componentType === "radar") {
+      var themeName = event.name;
+      new $tw.Story().navigateTiddler(themeName);
+      // console.log("On click: " + JSON.safeStringify(event));
+    }
   });
   return state;
 };
@@ -44,6 +63,103 @@ exports.onUpdate = function (echart) {
     }
   );
 
+  var themeRisks = [];
+  $tw.utils.each(
+    $tw.wiki.filterTiddlers(
+      "[twsm_class[theme]twsm_security_score[]twsm_json_field:max_risk_score[]]"
+    ),
+    function (score) {
+      // We normalise
+      themeRisks.push(score * 10.0);
+    }
+  );
+
+  var themeImpacts = [];
+  $tw.utils.each(
+    $tw.wiki.filterTiddlers(
+      "[twsm_class[theme]twsm_security_score[]twsm_json_field:max_impact[]]"
+    ),
+    function (score) {
+      // We normalise
+      themeImpacts.push(score * 20.0);
+    }
+  );
+
+  var scoreSeries = {
+    type: "radar",
+    data: [
+      {
+        value: themeScores,
+        name: "Score (0 = bad, 100 = good)",
+        symbol: "circle",
+        symbolSize: 12,
+        lineStyle: {
+          type: "dashed",
+        },
+        areaStyle: {
+          color: "#A0A0CAA0",
+        },
+        label: {
+          show: true,
+          formatter: function (p) {
+            console.log(JSON.safeStringify(p));
+              return p.value;
+          }
+        },
+      },
+    ],
+  }
+  var riskSeries = {
+    type: "radar",
+    data: [
+      {
+        value: themeRisks,
+        name: "Maximum risk (0 = good, 10 = bad)",
+        symbol: "circle",
+        symbolSize: 12,
+        lineStyle: {
+          type: "dashed",
+        },
+        areaStyle: {
+          color: "#C0A0A0A0",
+        },
+        label: {
+          show: true,
+          formatter: function (p) {
+            console.log(JSON.safeStringify(p));
+              return p.value / 10;
+          }
+        },
+      },
+    ],
+  }
+  var impactSeries = {
+    type: "radar",
+    data: [
+      {
+        value: themeImpacts,
+        name: "Maximum impact (0 -> 5)",
+        symbol: "circle",
+        symbolSize: 12,
+        lineStyle: {
+          type: "dashed",
+        },
+        areaStyle: {
+          color: "#A0C0A0A0",
+        },
+        label: {
+          show: true,
+          formatter: function (p) {
+            console.log(JSON.safeStringify(p));
+              return p.value / 20;
+          }
+        },
+      },
+    ],
+  }
+
+
+
   var isDarkMode = echart.getOption();
   isDarkMode = isDarkMode ? isDarkMode.darkMode !== false : false;
   echart.setOption({
@@ -51,9 +167,10 @@ exports.onUpdate = function (echart) {
       right: "10%",
       top: "3%",
     },
-    color: ["#0000FF"],
+    color: ["#00FF00", "#FF0000", "#0000FF"],
     radar: [
       {
+        triggerEvent: true,
         shape: "circle",
         indicator: indicatorDefinitions,
         center: ["50%", "50%"],
@@ -67,26 +184,7 @@ exports.onUpdate = function (echart) {
       },
     ],
     series: [
-      {
-        type: "radar",
-        data: [
-          {
-            value: themeScores,
-            name: "Score (0 = bad, 100 = good)",
-            symbol: "circle",
-            symbolSize: 12,
-            lineStyle: {
-              type: "dashed",
-            },
-            areaStyle: {
-              color: "#A0A0CAA0",
-            },
-            label: {
-              show: true,
-            },
-          },
-        ],
-      },
+      impactSeries, riskSeries, scoreSeries,
     ],
   });
 };
